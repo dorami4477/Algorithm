83ac714f536aceed73731dcde2ddf55f93beddb3
dorami4477 <85213387+dorami4477@users.noreply.github.com>
2024-11-14 18:00:22 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 376 ms, Memory: 93900 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.swift
@@ -0,0 +1,27 @@
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let n = input[0], m = input[1], r = input[2]
+var visited = [Int](repeating: 0, count: n + 1)
+var graph = [[Int]](repeating: [], count: n + 1)
+
+for _ in 0..<m {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    let u = input[0], v = input[1]
+    graph[u].append(v)
+    graph[v].append(u)
+}
+
+var depth = 1
+
+func dfs(node: Int) {
+    visited[node] = depth
+    for nextNode in graph[node].sorted(by: <) {
+        if visited[nextNode] == 0 {
+            depth += 1
+            dfs(node: nextNode)
+        }
+    }
+}
+
+dfs(node: r)
+
+print(visited[1...].map { String($0) }.joined(separator: "\n"))
