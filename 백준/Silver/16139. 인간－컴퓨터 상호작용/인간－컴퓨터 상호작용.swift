b1d0bed227201398642fba211cfb8792b0ac3ec9
Dahyun Park <85213387+dorami4477@users.noreply.github.com>
2025-01-09 17:55:58 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 900 ms, Memory: 389852 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.swift
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.swift
@@ -1,16 +1,30 @@
     let words = readLine()!.map { String($0) }
     let n = Int(readLine()!)!
+    var stringMap = [["a": 0]]
+    
+    for i in 1...words.count {
+        stringMap.append(stringMap[i - 1])
+        if stringMap[i][words[i - 1]] != nil {
+            stringMap[i][words[i - 1]]! += 1
+        } else {
+            stringMap[i][words[i - 1]] = 1
+        }
+    }
 
     for _ in 0..<n {
         let coord = readLine()!.split(separator: " ").map{ String($0) }
         let letter = coord[0]
         let start = Int(coord[1])!
         let end = Int(coord[2])!
-        var count = 0
-        for i in start...end {
-            if words[i] == letter {
-                count += 1
-            }
+        
+        var count = (stringMap[end + 1][letter] ?? 0) - (stringMap[start + 1][letter] ?? 0)
+        if words[start] == letter {
+            count += 1
         }
-        print(count)
+        
+        if count > 0 {
+            print(count)
+        }else {
+            print(0)
+        }
     }
