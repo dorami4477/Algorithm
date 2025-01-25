ff89c98dbaf0576bf93645afabd185db717bbfc5
Dahyun Park <85213387+dorami4477@users.noreply.github.com>
2025-01-25 22:03:20 +0900

    [Silver I] Title: 신입 사원, Time: 232 ms, Memory: 125520 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.swift
@@ -0,0 +1,74 @@
+import Foundation
+
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
+
+let file = FileIO()
+
+let testCase = file.readInt()
+for _ in 0..<testCase {
+    let n = file.readInt()
+    var grades = [Int](repeating: 0, count: n + 1)
+    var count = 1
+    
+    for _ in 0..<n {
+        let a = file.readInt(), b = file.readInt()
+        grades[a] = b
+    }
+    
+    var simpleGrade = grades[1]
+    
+    for i in 2...n {
+        if simpleGrade > grades[i] {
+            count += 1
+            simpleGrade = grades[i]
+        }
+    }
+    
+    print(count)
+}
