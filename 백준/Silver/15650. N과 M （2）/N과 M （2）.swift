df9ac71d58730b7cf3e0664a9a7b142f8f1b7bf0
Dahyun Park <85213387+dorami4477@users.noreply.github.com>
2025-02-17 17:06:28 +0900

    [Silver III] Title: N과 M (2), Time: 88 ms, Memory: 69120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.swift
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.swift
@@ -1,38 +1,32 @@
     let input = readLine()!.split(separator: " ").map{ Int($0)! }
     let n = input[0], m = input[1]
     var graph = [[Int]](repeating: [], count: n + 1)
-    
     for i in 1...n {
         for j in 1...n {
-            if j == i { continue }
+            if i == j { continue }
             graph[i].append(j)
         }
     }
-
-    var answer: Set<String> = []
     
-    for i in 1...n {
-        dfs(i, count: 0, group: [i])
-    }
+    var nums: Set<String> = []
     
-    func dfs(_ n: Int, count: Int, group: [Int]) {
-        var depth = count
-        depth += 1
+    func dfs(num: Int, group: [Int]) {
         var group = group
-        if depth == m {
-            let a  = group.sorted().map{ String($0) }.joined(separator: " ")
-            answer.insert(a)
+        if group.count == m {
+            nums.insert(group.sorted().map{ String($0) }.joined(separator: " "))
         }
         
-        for i in graph[n] {
+        for i in graph[num] {
             if !group.contains(i) {
                 group.append(i)
-                dfs(i, count: depth, group: group)
+                dfs(num: i, group: group)
                 group.removeLast()
             }
         }
     }
-    
-    for i in answer.sorted() {
-        print(i)
+
+    for i in 1...n {
+        dfs(num: i, group: [i])
     }
+    
+    nums.sorted().forEach { print($0) }
